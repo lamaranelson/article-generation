1bb527a8a13f8b0bf451bbfe0321fc04a2b889d6
lamaranelson <65568691+lamaranelson@users.noreply.github.com>
2023-10-13 18:23:29 -0700

    Update generateBlog.mjs

--- generateBlog.mjs
+++ generateBlog.mjs
@@ -147,7 +147,7 @@ async function generateBlog(userRequest) {
 }
 
 if (import.meta.url === `file://${process.argv[1]}`) {
-  const userInput = "I want to write a short blog post about the Amazon Forest";
+  const userInput = "I want to write a short blog post about the Amazon Jungle";
 
   generateBlog(userInput);
 }
